b51aa0c02ecf8b77dca6a27db77f5a1913b855ba
Eric Kidder <ekidder@salesforce.com>
2018-07-10 00:12:46 -0400

    Heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/Aello'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Aello/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
